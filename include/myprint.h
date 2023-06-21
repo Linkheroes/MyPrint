55a4a204e5476390a50bdb115ee6316ec0cc9ca4
Alexandre Ricard <alexandre.ricard@epitech.eu>
2023-06-21 19:15:27 +0200

    ADD: print_all in header file

--- include/myprint.h
+++ include/myprint.h
@@ -27,5 +27,6 @@ void print_error(char const *buffer);
 void print_char(char const c);
 void print_array(char **array);
 void print_nbr(int nb);
+void print_all(char const *format, ...);
 
 #endif /* !MYPRINT_H_ */
